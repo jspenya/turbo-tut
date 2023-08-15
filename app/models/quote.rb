4b03a51c6c6998408f4091973d863a485a0b604a
Jose Stephen Peña <pena.jstephen@gmail.com>
2023-08-16 01:43:29 +0800

    turbo stream broadcast to quotes/show

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -6,6 +6,10 @@ class Quote < ApplicationRecord
   # This is a shorter way to write the broadcast callbacks (create, update, destroy)
   broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
 
+  # Broadcast updates to quotes/show also
+  after_update_commit -> { broadcast_replace_later_to "quote", partial: "quotes/show_quote",
+    locals: { quote: self }, target: "quote_#{self.id}" }
+
   # after_create_commit -> { broadcast_prepend_later_to "quotes", partial: "quotes/quote",
   #   locals: { quote: self }, target: "quotes" }
   # after_update_commit -> { broadcast_replace_later_to "quotes", partial: "quotes/quote",
